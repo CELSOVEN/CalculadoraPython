42d90182727c831624cd274e49f6ed6e4dec1f7c
CELSO VENTURA MONTIEL HERNANDEZ <celsoven@hotmail.com>
2026-07-05 21:36:52 -0600

    Se actualiza app.py con código correcto

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-
+from flask import Flask, request
 from flask import Flask, render_template
+from os import system
+
+system("cls")
 
 app = Flask(__name__)
 
-@app.route("/")
-
-#Flask enviará el contenido de index.html
+@app.route("/", methods=["GET", "POST"])
 
 def inicio():
-    return render_template("index.html")
+    if request.method == "POST":
+        valor1 = request.form["valor1"]
+        valor2 = request.form["valor2"]
+        resultado = float(valor1) + float(valor2) 
+                       # Process the submitted values
+        return f"Values submitted: {valor1}, {valor2}. \
+        <br> <br> Sum Result: {resultado}"
+    return render_template("index2.html")
 
-app.run()
+if __name__ == "__main__":
+        app.run()
+
 
 
