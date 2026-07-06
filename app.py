3477b9be0bac76f002a36a69d4ce84652beeaf26
CELSO VENTURA MONTIEL HERNANDEZ <celsoven@hotmail.com>
2026-07-06 17:27:03 -0600

    Actualización de operaciones

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
+import module as md
 
 from colorama import init
 from flask import Flask, request
 from flask import Flask, render_template
-from os import system
 
-system("cls")
+os.system("cls")
 
 app = Flask(__name__)
 
@@ -15,17 +15,18 @@ def inicio():
     if request.method == "POST":
         valor1 = request.form["valor1"]
         valor2 = request.form["valor2"]
-        resultado = float(valor1) + float(valor2) 
-        multiplicacion = float(valor1) * float(valor2)
-                       # Process the submitted values
-        return f"Values submitted: {valor1}, {valor2}. \
-        <br> <br> Sum Resultado de suma: {resultado}. \
-        <br> <br> Multiplication Result: {multiplicacion}"
+        resultados = md.calcula_con_dos_numeros(float(valor1), float(valor2))
+        html_results = md.generate_html_results(resultados)
+        return html_results
     return render_template("index2.html")
 
+#Cuándo se trabaja localmente se utiliza esta línea de código
+#app.run()
+
+#Cuando se trabaja en la nube se utilizan estas líneas de código
 if __name__ == "__main__":
         app.run(
-             host = "0.0.0.0",
+            host = "0.0.0.0",
              port = init(os.environ.get("PORT", 5000)),
              debug = False             
         )
---
+++ module.py
@@ -0,0 +1,30 @@
+## Módulo para hacer operaciones
+
+def msg_resultado(resultado, operacion):
+    return(f"El resultado de la {operacion} es: {resultado}")
+
+
+def calcula_con_dos_numeros(valor1, valor2):
+    suma = valor1 + valor2
+    resta = valor1 - valor2
+    multiplicacion = valor1 * valor2
+    modulo = valor1 % valor2 if valor2 != 0 else "No se puede calcular el módulo entre cero"
+    potencia = valor1 ** valor2
+    division = valor1 / valor2 if valor2 != 0 else "No se puede dividir entre cero"
+    division_entera = valor1 // valor2  if valor2 != 0 else "No se puede dividir entre cero"
+    resultados = []
+    resultados.append(msg_resultado(suma, "Suma"))
+    resultados.append(msg_resultado(resta, "Resta"))
+    resultados.append(msg_resultado(multiplicacion, "Multiplicación"))
+    resultados.append(msg_resultado(modulo, "Módulo"))
+    resultados.append(msg_resultado(potencia, "Potencia"))
+    resultados.append(msg_resultado(division, "División"))
+    resultados.append(msg_resultado(division_entera, "División entera"))
+    return resultados 
+
+def generate_html_results(resultados):
+    html = "<ul>"
+    for resultado in resultados:
+        html += f"<li>{resultado}</li>"
+    html += "</ul>"
+    return html
